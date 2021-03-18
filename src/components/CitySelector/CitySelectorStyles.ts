02066d7b02296c040b22b7c6dc94431f597c6b15
Sebastián Alberto López <lopez.sebastiann.alberto@gmail.com>
2021-03-18 17:21:15 -0300

    CitySelector.tsx improvements

--- src/components/CitySelector/CitySelectorStyles.ts
+++ src/components/CitySelector/CitySelectorStyles.ts
@@ -1,13 +1,31 @@
 import styled from "styled-components";
 
 const CitySelectorStyles = styled.div`
-    input {
-        margin-right: .5em;
-        width: auto;
-        padding: .375rem .4rem;
-    }
-    button {
-        padding: .375rem .3rem;
+    display: flex;
+    flex: 1;
+
+    form.form-inline {
+        display: flex;
+        flex-flow: row wrap;
+        align-items: center;
+        flex: 1;
+        justify-content: flex-end;
+
+        input {
+            width: auto;
+        }
+        button {
+            margin-left: .5em;
+        }
+    
+        @media (max-width: 768px) {
+            input {
+                width: 100%;
+            }
+            button {
+                display: none;
+            }
+        }
     }
 `
 
